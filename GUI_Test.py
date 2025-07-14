9b80e37513b3ffd9372313141e7d8c0681279108
Gabriel <gabrielcoto.cr@gmail.com>
2025-07-14 13:46:35 -0600

    Commit Inicial
    Prueba de 'Hola Mundo' con el tkinter

---
+++ GUI_Test.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+
+root = tk.Tk()
+root.title('Menú Principal')
+
+window_width = 400
+window_height = 600
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+root.resizable(False, False)
+# place a label on the root window
+message = tk.Label(root, text="Hello, World!")
+message.pack()
+
+# keep the window displaying
+root.mainloop()
